fcb51e11615be7f0f35a830dce58f15e07609b1e
Richard Kielty <rich.kielty@gmail.com>
2025-10-28 23:24:40 +0000

    Removed extra export, added Message export for event handler

--- library/queue/src/index.ts
+++ library/queue/src/index.ts
@@ -1,3 +1,4 @@
+export { Message } from "@aws-sdk/client-sqs";
 export { receiveMessages } from './sqs-receive-messages.js';
 export { sendMessage } from './sqs-send-message.js';
 export { Secret } from './types.js';
--- worker/user-service-queue/src/event-handler/user-registered.ts
+++ worker/user-service-queue/src/event-handler/user-registered.ts
@@ -1,11 +1,11 @@
 import { logger } from "@tw050x.net.library/logger"
-import { default as DisposableMessage } from "@tw050x.net.library/queue";
+import { Message } from "@tw050x.net.library/queue";
 
 /**
  * Handles a UserRegistered event message.
  *
- * @param message - The disposable message containing the UserRegistered event.
+ * @param message - The message containing the UserRegistered event.
  */
-export default async function handleUserRegisteredEvent(message: DisposableMessage): Promise<void> {
+export default async function handleUserRegisteredEvent(message: Message): Promise<void> {
   logger.debug('Handling UserRegistered event with body:', message);
 }
