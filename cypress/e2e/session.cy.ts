6d0292bc1e0e47e22bbbcca0b9510e385197b91a
Richard Kielty <rich.kielty@gmail.com>
2025-12-04 19:46:35 +0000

    Add session cypress tests

---
+++ cypress/e2e/session.cy.ts
@@ -0,0 +1,66 @@
+describe('As a user when I am logged in, I want my session to refresh with activity', () => {
+  it('should refresh session every time there is activity from me', () => {
+    const timestamp = Date.now();
+    const email = `test.user.${timestamp}@example.com`;
+    const password = 'Password123!';
+    cy.task<{ id: string }>('createUserAndSession', { email, password }).then((session) => {
+      cy.setCookie('user.session', session.id, {
+        domain: 'tw050x.dev',
+        httpOnly: false,
+        expiry: Math.floor(Date.now() / 1000) + 3600,
+        path: '/',
+        sameSite: 'lax',
+        secure: true,
+      });
+    });
+
+    cy.intercept('GET', '/portal/dashboard').as('dashboardPageLoad');
+    cy.visit('/portal/dashboard');
+
+    let expiryOneMatch;
+    cy.wait('@dashboardPageLoad').then((interception) => {
+      expect(interception.response?.statusCode).to.eq(200);
+      expect(interception.response?.headers).to.have.property('set-cookie');
+      const setCookieHeader = interception.response?.headers['set-cookie'];
+      const sessionCookie = Array.isArray(setCookieHeader)
+        ? setCookieHeader.find((cookie) => cookie.startsWith('user.session='))
+        : setCookieHeader?.startsWith('user.session=')
+          ? setCookieHeader
+          : undefined;
+
+      expect(sessionCookie).to.exist;
+      expiryOneMatch = sessionCookie?.match(/expires=([^;]+)/);
+      expect(expiryOneMatch).to.exist;
+    });
+
+    cy.wait(2000); // wait for 2 seconds to ensure a different expiry time
+
+    cy.intercept('GET', '/portal/settings').as('settingsPageLoad');
+    cy.visit('/portal/settings');
+
+    let expiryTwoMatch;
+    cy.wait('@settingsPageLoad').
+      then((interception) => {
+        expect(interception.response?.statusCode).to.eq(200);
+        expect(interception.response?.headers).to.have.property('set-cookie');
+        const setCookieHeader = interception.response?.headers['set-cookie'];
+        const sessionCookie = Array.isArray(setCookieHeader)
+          ? setCookieHeader.find((cookie) => cookie.startsWith('user.session='))
+          : setCookieHeader?.startsWith('user.session=')
+            ? setCookieHeader
+            : undefined;
+
+        expect(sessionCookie).to.exist;
+        expiryTwoMatch = sessionCookie?.match(/expires=([^;]+)/);
+        expect(expiryTwoMatch).to.exist;
+      }).
+      then(() => {
+        expect(expiryOneMatch[0]).to.not.eq(expiryTwoMatch[0]);
+      });
+  });
+
+  it('should redirect to the login page when there is no active session',  () => {
+    cy.visit('/portal/dashboard');
+    cy.url().should('eq', `${Cypress.config().baseUrl}/login`);
+  });
+});
